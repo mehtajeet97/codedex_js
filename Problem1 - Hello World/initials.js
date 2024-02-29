c00c1245a2a265abc38b89bd5ea06b80c9d282c5
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-29 03:32:57 -0500

    Problem1 completed

---
+++ Problem1 - Hello World/initials.js
@@ -0,0 +1,10 @@
+// This is how you comment in a single line in JS
+console.log("JJJJJJJ  MM   MM");
+console.log("   J     M M M M");
+console.log("   J     M  M  M");
+console.log("   J     M  M  M");
+console.log("J  J     M     M");
+console.log("JJJJ     M     M");
+
+/* This is how you do
+a multiline comment in JS*/
