2c88d0c83298ae662b9e3e91b872f3c3b76b0c6f
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-03 21:31:27 -0400

    Problem6 added

---
+++ Problem6/variables.js
@@ -0,0 +1,8 @@
+const id = 123;
+let name = "Darth Vader";
+let stars = 1;
+const starName = "Death Star";
+let heroesVictory = true;
+stars = 3;
+
+console.log(id, name, stars, starName, heroesVictory);
