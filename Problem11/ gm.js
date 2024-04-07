10984136d911eb5eb6bc71c212eeb7f66abaf885
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-07 19:27:42 -0400

    Problem 13 completed

--- Problem11/ gm.js
+++ Problem11/ gm.js
@@ -1,7 +1,9 @@
-// Problem 12 : Good Morning
+// Problem 12+13 : Good Morning & Good Afternoon
 
 let hour = 7;
 
 if (hour < 12) {
   console.log("Good morning 🌞");
+} else {
+  console.log("Good afternoon ☁️");
 }
