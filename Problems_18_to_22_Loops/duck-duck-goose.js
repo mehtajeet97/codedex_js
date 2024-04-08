a502e67034271d5cebe4b1c2fd8880dce20a272a
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-07 20:54:27 -0400

    Problem 18 completed

---
+++ Problems_18_to_22_Loops/duck-duck-goose.js
@@ -0,0 +1,10 @@
+// Problem 18 : Duck Duck Goose
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber != 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🦢");
