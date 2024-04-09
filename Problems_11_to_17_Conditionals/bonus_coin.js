a1f1736c204b9dc0dd969715745accf0a46e557d
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-08 21:14:54 -0400

    Bonus article problem added

---
+++ Problems_11_to_17_Conditionals/bonus_coin.js
@@ -0,0 +1,6 @@
+// Bonus article : Ternary operators
+
+let num = Math.random();
+
+// Single line ternary operator used instead of if-else statement
+num > 0.5 ? console.log("Heads") : console.log("Tails");
