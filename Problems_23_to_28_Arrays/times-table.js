89637e53c6864188f8febc6d599b932ecb03261b
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-08 23:55:49 -0400

    Problem 25 completed

---
+++ Problems_23_to_28_Arrays/times-table.js
@@ -0,0 +1,8 @@
+// Problem 25 : Times Table
+
+let multiple = 9;
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`${multiple} * ${numbers[i]} = ` + multiple * numbers[i]);
+}
