26c92e1f0f026daa3bbab325a524342098acf1f0
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-08 22:55:50 -0400

    Problem 24 completed

---
+++ Problems_23_to_28_Arrays/grocery.js
@@ -0,0 +1,9 @@
+// Problem 24 : Grocery
+
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"];
+
+groceryList[2] = "🧈 Butter";
+
+groceryList[4] = "🧼 Laundry Soap";
+
+console.log(groceryList);
