f877ba0b0ce55ef42a87d27f0e776392ceaba87a
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-09 23:59:44 -0400

    Problem 32 created

---
+++ Problems_29_to_33_Functions/clout.js
@@ -0,0 +1,13 @@
+//Problem 32 : Clout
+
+const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
+const recentInstagramViews = [936, 2300, 453, 5222, 6733, 7402, 8334];
+const recentYouTubeViews = [989, 2300, 453, 5222, 6733, 7402, 8334];
+
+function mean(viewsArray) {
+  let totalViews = 0;
+}
+
+function median(viewsArray) {}
+
+console.log(recentInstagramViews + recentTikTokViews + recentYouTubeViews);
