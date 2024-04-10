6c9b082ecf5b6a357a6e09f63cc31c1188452d56
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-10 00:30:19 -0400

    Problem 32 completed

--- Problems_29_to_33_Functions/clout.js
+++ Problems_29_to_33_Functions/clout.js
@@ -6,8 +6,33 @@ const recentYouTubeViews = [989, 2300, 453, 5222, 6733, 7402, 8334];
 
 function mean(viewsArray) {
   let totalViews = 0;
+  for (let i = 0; i < viewsArray.length; i++) {
+    totalViews += viewsArray[i];
+  }
+  return totalViews / viewsArray.length;
 }
 
-function median(viewsArray) {}
+function median(viewsArray) {
+  let sortedStats = [];
+  let middleIndex = 0;
+  sortedStats = viewsArray.sort(function (a, b) {
+    return a - b;
+  });
+  if (sortedStats.length % 2 !== 0) {
+    middleIndex = Math.floor(sortedStats.length / 2);
+    return sortedStats[middleIndex];
+  } else {
+    middleIndex = sortedStats.length / 2;
+    return mean([sortedStats[middleIndex], sortedStats[middleIndex - 1]]);
+  }
+}
 
-console.log(recentInstagramViews + recentTikTokViews + recentYouTubeViews);
+// Mean
+console.log(mean(recentInstagramViews));
+console.log(mean(recentTikTokViews));
+console.log(mean(recentYouTubeViews));
+
+// Median
+console.log(median(recentInstagramViews));
+console.log(median(recentTikTokViews));
+console.log(median(recentYouTubeViews));
