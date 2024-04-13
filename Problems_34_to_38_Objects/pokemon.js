f952ce1057da36d60c2eadddc650acbec4d041c7
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-12 21:29:26 -0400

    Problem 36 completed

---
+++ Problems_34_to_38_Objects/pokemon.js
@@ -0,0 +1,18 @@
+// Problem 36 : Pokemon
+
+pokemon = {
+  name: "Jigglypuff",
+  type: "Fairy",
+  level: 25,
+};
+
+pokemon.isCaught = false;
+
+console.log(pokemon);
+
+// After catching
+
+pokemon.isCaught = true;
+pokemon.name = "Jiggly";
+
+console.log(pokemon);
