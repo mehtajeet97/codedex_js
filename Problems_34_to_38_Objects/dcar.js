580eae723b4f2a2d6e54572d6bc070b6dd3aa953
Jeet Mehta <mehta.jeet97@gmail.com>
2024-04-12 21:25:04 -0400

    Problems 34-35 completed

---
+++ Problems_34_to_38_Objects/dcar.js
@@ -0,0 +1,12 @@
+// Problem 35 : Dream Car
+
+const car = {};
+
+car.model = "Tesla";
+car.year = 2019;
+car.color = "Black";
+car.new = false;
+
+console.log(
+  `I'm looking for a ${car.color} ${car.year} ${car.model} that is used.`
+);
