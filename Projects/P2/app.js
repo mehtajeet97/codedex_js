7effc8590c9b0457b3b31e70a5f7547e20323795
Jeet Mehta <mehta.jeet97@gmail.com>
2024-05-10 20:57:10 -0400

    Comment removed

--- Projects/P2/app.js
+++ Projects/P2/app.js
@@ -3,7 +3,6 @@ const startBtn = document.querySelector(".btn-start");
 const session = document.querySelector(".minutes");
 let myInterval;
 let state = true;
-// Check again in the morning, Or maybe in the Night and tonight as well
 
 const appTimer = () => {
   const sessionAmount = Number.parseInt(session.textContent);
