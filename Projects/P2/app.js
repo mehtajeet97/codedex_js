ecc848ce43df3c5b19a6c074d7bcb4fb3c0ab14d
Jeet Mehta <mehta.jeet97@gmail.com>
2024-05-16 23:03:27 -0400

    Comments updated

--- Projects/P2/app.js
+++ Projects/P2/app.js
@@ -3,7 +3,7 @@ const startBtn = document.querySelector(".btn-start");
 const session = document.querySelector(".minutes");
 let myInterval;
 let state = true;
-// About time this comment was removed
+// About time this comment was removed... But added another one
 const appTimer = () => {
   const sessionAmount = Number.parseInt(session.textContent);
 
