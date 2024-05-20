2a0d55738238722138de80e8390b06e58b3aebd9
Jeet Mehta <mehta.jeet97@gmail.com>
2024-05-19 22:22:44 -0400

    Comment

--- Projects/P2/app.js
+++ Projects/P2/app.js
@@ -3,7 +3,7 @@ const startBtn = document.querySelector(".btn-start");
 const session = document.querySelector(".minutes");
 let myInterval;
 let state = true;
-// About time this comment was removed... But added another one... And the saga continues... Forever
+// About time this comment was removed... But added another one... And the saga continues... Forever... What about the Promodoro App
 const appTimer = () => {
   const sessionAmount = Number.parseInt(session.textContent);
 
